fb9cb6370ce9790f170ff5f2756d9045c8a30fa9
NIGAM PRASAD SAHOO <nigam.nps@gmail.com>
2026-02-23 15:40:14 +0530

    2487. Remove Nodes From Linked List

---
+++ src/Stack/Remove_nodes_stack.java
@@ -0,0 +1,29 @@
+package Stack;
+
+import linkedList.ListNode;
+
+import java.util.Stack;
+
+public class Remove_nodes_stack {
+    class Solution {
+        public ListNode removeNodes(ListNode head) {
+            ListNode temp=head;
+            Stack<ListNode> st=new Stack<ListNode>();
+            while(temp!=null){
+                while (!st.isEmpty() && st.peek().val < temp.val) {
+                    st.pop();
+                }
+                st.push(temp);
+                temp = temp.next;
+            }
+
+            while(st.size()>0){
+                ListNode t=st.pop();
+                t.next=temp;
+                temp=t;
+            }
+            return temp;
+
+        }
+    }
+}
