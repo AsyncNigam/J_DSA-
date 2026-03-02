88e692abde96a1a9cbaee597c88c0c931399afa3
NIGAM PRASAD SAHOO <nigam.nps@gmail.com>
2026-03-02 23:51:31 +0530

    53. Maximum Subarray , with Kadane's algorithm

---
+++ src/Apna_clg_sheet_revision/maximum_subarray_sum.java
@@ -0,0 +1,36 @@
+package Apna_clg_sheet_revision;
+
+public class maximum_subarray_sum {
+
+//    this is the brute force appraoch but it has exceeding time complexity
+
+//    class Solution {
+//        public int maxSubArray(int[] nums) {
+//            int msum=nums[0];
+//            for(int i=0;i<nums.length;i++){
+//                int sum=0;
+//                for(int j=i;j<nums.length;j++){
+//                    sum+=nums[j];
+//                    msum=Math.max(msum,sum);
+//                }
+//            }
+//            return msum;
+//        }
+//    }
+
+
+
+//    with kadane's algorithm with time O(n)
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int max_sum=nums[0];
+        int current_sum=0;
+        for(int i=0;i<nums.length;i++){
+            current_sum+=nums[i];
+            max_sum=Math.max(max_sum,current_sum);
+            if(current_sum<0)current_sum=0;
+        }
+        return max_sum;
+    }
+}
+}
