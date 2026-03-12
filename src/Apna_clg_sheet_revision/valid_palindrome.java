e805cd31fdfdb051dd20e144575e53a54d167012
NIGAM PRASAD SAHOO <nigam.nps@gmail.com>
2026-03-12 12:43:54 +0530

    125. Valid Palindrome

---
+++ src/Apna_clg_sheet_revision/valid_palindrome.java
@@ -0,0 +1,35 @@
+package Apna_clg_sheet_revision;
+
+//https://leetcode.com/problems/valid-palindrome/
+
+
+//here we don't need to create new string or builder for checking just keeping the pointer and continuing is the key
+
+
+
+public class valid_palindrome {
+    class Solution {
+    public boolean isPalindrome(String s) {
+        int i=0;
+        int j=s.length()-1;
+        while(i<j){
+            char left=s.charAt(i);
+            char right=s.charAt(j);
+
+            if(!Character.isLetterOrDigit(left) || !Character.isLetterOrDigit(right)){
+                if(!Character.isLetterOrDigit(left))i++;
+                if(!Character.isLetterOrDigit(right))j--;
+            }
+
+           else if(Character.toLowerCase(left)!=Character.toLowerCase(right))return false;
+            else{
+                i++;
+                j--;
+            }
+
+        }
+        return true;
+
+    }
+}
+}
