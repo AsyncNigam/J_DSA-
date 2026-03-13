4e8bb2e5d0ff9324cd4665931bbad71828a56eb6
NIGAM PRASAD SAHOO <nigam.nps@gmail.com>
2026-03-14 02:55:39 +0530

    283. Move Zeroes

---
+++ src/Apna_clg_sheet_revision/move_zeros.java
@@ -0,0 +1,16 @@
+package Apna_clg_sheet_revision;
+
+public class move_zeros {
+    class Solution {
+        public void moveZeroes(int[] nums) {
+            int  c=0;
+            for(int i=0;i<nums.length;i++){
+                if(nums[i]!=0){
+                    nums[c++]=nums[i];
+                }
+            }
+            while(c<nums.length)nums[c++]=0;
+        }
+
+    }
+}
