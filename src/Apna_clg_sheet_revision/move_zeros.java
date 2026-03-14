efe10a4d77ba5767f73fcff57757a6a75b2816da
NIGAM PRASAD SAHOO <nigam.nps@gmail.com>
2026-03-15 00:05:00 +0530

    242. Valid Anagram

--- src/Apna_clg_sheet_revision/move_zeros.java
+++ src/Apna_clg_sheet_revision/move_zeros.java
@@ -1,16 +1,42 @@
 package Apna_clg_sheet_revision;
 
 public class move_zeros {
-    class Solution {
-        public void moveZeroes(int[] nums) {
-            int  c=0;
-            for(int i=0;i<nums.length;i++){
-                if(nums[i]!=0){
-                    nums[c++]=nums[i];
-                }
-            }
-            while(c<nums.length)nums[c++]=0;
-        }
 
+//     my approach is if nonZero then equate , then for the rest equals to 0
+
+//     "Overwrite" Approach
+
+//    class Solution {
+//        public void moveZeroes(int[] nums) {
+//            int  non0=0;
+//            for(int i=0;i<nums.length;i++){
+//                if(nums[i]!=0){
+//                    nums[non0++]=nums[i];
+//                }
+//            }
+//            while(non0<nums.length)nums[non0++]=0;
+//        }
+//    }
+
+//    other approach with the same time complexity SWAP APPROACH
+
+class Solution {
+    public void swap(int a,int b,int[] nums){
+        int temp=nums[a];
+        nums[a]=nums[b];
+        nums[b]=temp;
     }
+    public void moveZeroes(int[] nums) {
+        int n=nums.length;
+        int i=0;
+        int j=0;
+        while(j<n){
+            if(nums[j]!=0){
+                swap(i,j,nums);
+                i++;
+            }
+            j++;
+        }
+    }
+}
 }
---
+++ src/Apna_clg_sheet_revision/valid_anagram.java
@@ -0,0 +1,22 @@
+package Apna_clg_sheet_revision;
+
+import java.util.Arrays;
+
+public class valid_anagram {
+    class Solution {
+        public boolean isAnagram(String s, String t) {
+            char[] str=s.toCharArray();
+            char[] ttr=t.toCharArray();
+
+            if(str.length!=ttr.length)return false;
+            Arrays.sort(str);
+            Arrays.sort(ttr);
+
+            for(int i=0;i<str.length;i++){
+                if(str[i]!=ttr[i])return false;
+            }
+            return true;
+
+        }
+    }
+}
