776e553d819944af12b34c3c3be10ef28a951fa4
NIGAM PRASAD SAHOO <nigam.nps@gmail.com>
2026-04-05 23:30:55 +0530

    leetcode_257_robot_return_to_origin

---
+++ src/Apna_clg_sheet_revision/leetcode_257_robot_return_to_origin.java
@@ -0,0 +1,17 @@
+package Apna_clg_sheet_revision;
+
+public class leetcode_257_robot_return_to_origin {
+    class Solution {
+        public boolean judgeCircle(String moves) {
+            char[] arr=moves.toCharArray();
+            int num=0;
+            for(char ch:arr){
+                if(ch=='U')num+=1;
+                else if(ch=='D')num+=-1;
+                else if (ch=='L') num+=200;
+                else num+=-200;
+            }
+            return num==0;
+        }
+    }
+}
