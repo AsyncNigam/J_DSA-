abad1ce53bb6dd00b81cb4b45eb9058ff5fb7f3a
NIGAM PRASAD SAHOO <nigam.nps@gmail.com>
2026-04-22 17:02:32 +0530

    64. Minimum Path Sum

---
+++ src/DYNAMIC_PROGRAMMING/ques_09_minimum_path_sum.java
@@ -0,0 +1,25 @@
+package DYNAMIC_PROGRAMMING;
+
+public class ques_09_minimum_path_sum {
+//    by tabulation method
+//    in this question we do not need to create an extra array like previous question
+
+//    time complexity=O(m*n)
+//    space complexity=O(m*n)
+//    auxiliary space=O(n)
+class Solution {
+    public int minPathSum(int[][] grid) {
+        int m=grid.length;
+        int n=grid[0].length;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0&&j==0)continue;
+                else if(i==0)grid[i][j]+=grid[i][j-1];
+                else if(j==0)grid[i][j]+=grid[i-1][j];
+                else grid[i][j]+=Math.min(grid[i][j-1],grid[i-1][j]);
+            }
+        }
+        return grid[m-1][n-1];
+    }
+}
+}
