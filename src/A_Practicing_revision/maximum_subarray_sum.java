1d86cf96cf32665ddd730f3e3df46906801941d7
NIGAM PRASAD SAHOO <nigam.nps@gmail.com>
2026-05-20 23:17:50 +0530

    revised maximum subarray

--- src/A_Practicing_revision/maximum_subarray_sum.java
+++ src/A_Practicing_revision/maximum_subarray_sum.java
@@ -23,11 +23,11 @@ public class maximum_subarray_sum {
 //    with kadane's algorithm with time O(n)
 class Solution {
     public int maxSubArray(int[] nums) {
-        int max_sum=nums[0];
         int current_sum=0;
+        int max_sum=nums[0];
         for(int i=0;i<nums.length;i++){
             current_sum+=nums[i];
-            max_sum=Math.max(max_sum,current_sum);
+            max_sum=Math.max(current_sum,max_sum);
             if(current_sum<0)current_sum=0;
         }
         return max_sum;
