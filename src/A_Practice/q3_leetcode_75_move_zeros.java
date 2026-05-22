8e91cfe0bd22ae03247728f2d0510d8b3aff6b73
NIGAM PRASAD SAHOO <nigam.nps@gmail.com>
2026-05-22 09:52:37 +0530

    283. Move Zeroes

---
+++ src/A_Practice/q3_leetcode_75_move_zeros.java
@@ -0,0 +1,51 @@
+package A_Practice;
+
+public class q3_leetcode_75_move_zeros {
+
+//     my approach is if nonZero then equate , then for the rest equals to 0
+
+//     "Overwrite" Approach
+
+//    class Solution {
+//        public void moveZeroes(int[] nums) {
+//            int  non0=0;
+//            for(int i=0;i<nums.length;i++){
+//                if(nums[i]!=0){
+//                    nums[non0++]=nums[i];
+//                }
+//            }
+//            while(non0<nums.length)nums[non0++]=0;
+//        }
+//    }
+
+//    other approach with the same time complexity SWAP APPROACH
+
+//class Solution {
+//    public void swap(int a,int b,int[] nums){
+//        int temp=nums[a];
+//        nums[a]=nums[b];
+//        nums[b]=temp;
+//    }
+//    public void moveZeroes(int[] nums) {
+//        int n=nums.length;
+//        int i=0;
+//        int j=0;
+//        while(j<n){
+//            if(nums[j]!=0){
+//                swap(i,j,nums);
+//                i++;
+//            }
+//            j++;
+//        }
+//    }
+//}
+
+//    lettest approach of mine
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int non0=0;
+        for(int i=0;i<nums.length;i++)  if(nums[i]!=0) nums[non0++]=nums[i];
+        while(non0<nums.length)nums[non0++]=0;
+    }
+}
+}
