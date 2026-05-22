44b3773c466adfeb80779223d7bee841f5eb1843
NIGAM PRASAD SAHOO <nigam.nps@gmail.com>
2026-05-22 13:33:51 +0530

    Q1. Set Mismatch revised

--- src/A_Practice/q6_set_mismatch.java
+++ src/A_Practice/q6_set_mismatch.java
@@ -45,4 +45,42 @@ class Solution {
         return ans;
     }
 }
+
+
+//my old approach same time complexity o(n)
+
+
+
+//    class Solution {
+//    public int[] findErrorNums(int[] arr) {
+//
+//        int[] nums=new int[2];
+//    int i=0;
+//    while(i< arr.length){
+//            int correct=arr[i]-1;
+//
+//                if(arr[i]!=arr[correct]){
+//
+//                    swap(arr,i,correct);
+//
+//                }
+//                else   i++;
+//            }
+//    for(int j=0;j<arr.length;j++){
+//        if(arr[j]!=j+1){
+//            nums[0]=arr[j];
+//            nums[1]=j+1;
+//        }
+//    }
+//
+//    return nums;
+//    }
+//
+//    void swap(int arr[],int i,int correct ){
+//        int temp=arr[i];
+//        arr[i]=arr[correct];
+//        arr[correct]=temp;
+//
+//    }
+//}
 }
