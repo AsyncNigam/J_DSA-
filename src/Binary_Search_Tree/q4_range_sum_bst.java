0e7bbcc0452a61779013d19f3256e6e8aa712117
NIGAM PRASAD SAHOO <nigam.nps@gmail.com>
2026-05-26 01:09:53 +0530

    938. Range Sum of BST

---
+++ src/Binary_Search_Tree/q4_range_sum_bst.java
@@ -0,0 +1,14 @@
+package Binary_Search_Tree;
+
+//https://leetcode.com/problems/range-sum-of-bst/description/
+
+public class q4_range_sum_bst {
+    class Solution {
+        public int rangeSumBST(TreeNode root, int low, int high) {
+            if(root==null)return 0;
+            if(root.val<low)return rangeSumBST(root.right,low,high);
+            if(root.val>high)return rangeSumBST(root.left,low,high);
+            return root.val+rangeSumBST(root.left,low,high)+rangeSumBST(root.right,low,high);
+        }
+    }
+}
