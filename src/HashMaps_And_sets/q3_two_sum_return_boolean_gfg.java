6b534ee0c86e85931fbb819b2ee31ecd12a4b9fc
NIGAM PRASAD SAHOO <nigam.nps@gmail.com>
2026-06-05 02:36:35 +0530

    two_sum_return_boolean_gfg

---
+++ src/HashMaps_And_sets/q3_two_sum_return_boolean_gfg.java
@@ -0,0 +1,19 @@
+package HashMaps_And_sets;
+
+import java.util.HashSet;
+//https://www.geeksforgeeks.org/problems/key-pair5616/1
+public class q3_two_sum_return_boolean_gfg {
+    class Solution {
+        boolean twoSum(int arr[], int target) {
+            // code here
+            HashSet<Integer> set=new HashSet<Integer>();
+            for(int el:arr){
+                if(set.contains(target-el))return true;
+                set.add(el);
+
+            }
+            return false;
+
+        }
+    }
+}
