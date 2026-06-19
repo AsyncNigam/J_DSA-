b2403abbd60f8aba3847eba1770d7a3a255b5ae5
NIGAM PRASAD SAHOO <nigam.nps@gmail.com>
2026-06-19 23:59:56 +0530

    n

--- src/Binary_Search_Tree/q3_search_in_bst.java
+++ src/Binary_Search_Tree/q3_search_in_bst.java
@@ -6,6 +6,7 @@ package Binary_Search_Tree;
 //in worst case time complexity is O(n) worst case is like single lined bst
 public class q3_search_in_bst {
     class Solution {
+
         public TreeNode searchBST(TreeNode root, int val) {
             if(root==null)return null;
             if(root.val==val)return root;
