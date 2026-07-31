8a8ef88534e98ba2867abcf8be7d95a0bffe219a
NIGAM PRASAD SAHOO <nigam.nps@gmail.com>
2026-08-01 01:09:45 +0530

    Revision: sieve of eratosthenes approach count primes

---
+++ src/Zeus_stroke/q1_count_primes.java
@@ -0,0 +1,45 @@
+package Zeus_stroke;
+
+public class q1_count_primes {
+
+    public class leetcode_204 {
+
+//    brute force approach , but this has time limit exceeded
+
+//    class Solution {
+//        public int countPrimes(int n) {
+//            int c=0;
+//            for(int i=0;i<n;i++){
+//                if(isPrime(i))c++;
+//            }
+//            return c;
+//        }
+//         boolean isPrime(int n){
+//        if(n<2)return false;
+//        for(int i=2;i*i<=n;i++){
+//            if(n%i==0)return false;
+//        }
+//        return true;
+//    }
+//    }
+
+        //    sieve of eratosthenes approach
+        class Solution {
+            public int countPrimes(int n) {
+                int[] prime=new int[n];
+                int c=0;
+                for(int i=2;i<n;i++){
+                    if(prime[i]==0){
+                        c++;
+                        for(long j=(long)i*i;j<n;j+=i){
+                            prime[(int)j]=1;
+                        }
+                    }
+                }
+                return c;
+            }
+        }
+
+    }
+
+}
