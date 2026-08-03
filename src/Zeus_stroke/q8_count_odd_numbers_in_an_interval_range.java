8ebe44ec378a91da0c486eed7fe90cb94c479e5b
NIGAM PRASAD SAHOO <nigam.nps@gmail.com>
2026-08-03 23:14:02 +0530

    1523. Count Odd Numbers in an Interval Range

---
+++ src/Zeus_stroke/q8_count_odd_numbers_in_an_interval_range.java
@@ -0,0 +1,25 @@
+package Zeus_stroke;
+
+public class q8_count_odd_numbers_in_an_interval_range {
+//    https://leetcode.com/problems/count-odd-numbers-in-an-interval-range/description/
+
+//    my brute force approach is
+class Solution {
+    public int countOdds(int low, int high) {int c=0;
+        for(int i=low;i<=high;i++){
+            if(i%2!=0)c++;
+        }
+        return c;
+    }
+}
+
+//but the constraint is 10^9
+
+//    more optimized approach is
+class Solution2 {
+    public int countOdds(int low, int high) {
+
+        return (high+1)/2 - low/2;
+    }
+}
+}
