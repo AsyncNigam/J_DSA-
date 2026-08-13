02b9d9fd77874f43867398c19eeccba6b315f324
NIGAM PRASAD SAHOO <nigam.nps@gmail.com>
2026-08-14 02:15:47 +0530

    3005. Count Elements With Maximum Frequency

---
+++ src/Zeus_stroke/q5_hashmap/q1_count_elements_with_maximum_frequencies.java
@@ -0,0 +1,25 @@
+package Zeus_stroke.q5_hashmap;
+
+import java.util.HashMap;
+
+public class q1_count_elements_with_maximum_frequencies {
+    class Solution {
+        public int maxFrequencyElements(int[] nums) {
+            HashMap<Integer, Integer> map=new HashMap<>();
+            for(int i=0;i<nums.length;i++){
+                map.put(nums[i],map.getOrDefault(nums[i], 0) + 1);
+            }
+            int max_len=0;
+            int mVal=0;
+
+            for(int i:map.keySet()){
+                if(max_len<map.get(i)){
+                    max_len=map.get(i);
+                    mVal=max_len;
+                }
+                else if(max_len == map.get(i))mVal+=max_len;
+            }
+            return mVal;
+        }
+    }
+}
