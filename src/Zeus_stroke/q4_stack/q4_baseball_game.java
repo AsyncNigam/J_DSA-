8865b23e0173f7b21cf6e8ce6593ee728a0af460
NIGAM PRASAD SAHOO <nigam.nps@gmail.com>
2026-08-15 23:54:15 +0530

    682. Baseball Game

---
+++ src/Zeus_stroke/q4_stack/q4_baseball_game.java
@@ -0,0 +1,36 @@
+package Zeus_stroke.q4_stack;
+
+import java.util.Stack;
+
+public class q4_baseball_game {
+    class Solution {
+        public int calPoints(String[] operations) {
+            Stack<String> st = new Stack<>();
+
+            for (int i = 0; i < operations.length; i++) {
+                if (operations[i].equals("+")) {
+                    int top = Integer.parseInt(st.pop());
+                    int top2 = Integer.parseInt(st.pop());
+                    int res = top + top2;
+                    st.push(String.valueOf(top2));
+                    st.push(String.valueOf(top));
+                    st.push(String.valueOf(res));
+                } else if (operations[i].equals("D")) {
+                    int top = Integer.parseInt(st.peek());
+                    st.push(String.valueOf(top * 2));
+                } else if (operations[i].equals("C")) {
+                    st.pop();
+                } else {
+                    // It's a number (handles positive, negative, any value)
+                    st.push(operations[i]);
+                }
+            }
+
+            int sum = 0;
+            while (!st.isEmpty()) {
+                sum += Integer.parseInt(st.pop());
+            }
+            return sum;
+        }
+    }
+}
