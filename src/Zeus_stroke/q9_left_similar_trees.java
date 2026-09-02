3147ca20d3d3e51edfb2830a1b8fc52aa962456b
NIGAM PRASAD SAHOO <nigam.nps@gmail.com>
2026-09-02 11:38:48 +0530

    872. Leaf-Similar Trees

---
+++ src/Zeus_stroke/q9_left_similar_trees.java
@@ -0,0 +1,29 @@
+package Zeus_stroke;
+
+import Binary_Search_Tree.TreeNode;
+
+import java.util.ArrayList;
+
+public class q9_left_similar_trees {
+    class Solution {
+        public boolean leafSimilar(TreeNode root1, TreeNode root2) {
+            ArrayList<Integer> arr1=new ArrayList<>();
+            count(root1,arr1);
+
+            ArrayList<Integer> arr2=new ArrayList<>();
+            count(root2,arr2);
+
+            return arr1.equals(arr2);
+        }
+        void count(TreeNode root,ArrayList<Integer> arr){
+            if(root==null)return;
+
+            if(root.left==null && root.right==null) arr.add(root.val);
+
+            count(root.left,arr);
+            count(root.right, arr);
+        }
+
+
+    }
+}
