8fc22c07bc98c89971cab36d49c0068ee0ff7299
NIGAM PRASAD SAHOO <nigam.nps@gmail.com>
2026-09-07 15:35:05 +0530

    112. Path Sum

---
+++ src/Zeus_stroke/binary_trees/q2_path_sum.java
@@ -0,0 +1,15 @@
+package Zeus_stroke.binary_trees;
+
+public class q2_path_sum {
+    class Solution {
+        public boolean hasPathSum(TreeNode root, int targetSum) {
+            if(root==null)return false;
+            if(root.left==null && root.right==null){
+                if(root.val==targetSum)return true;
+                else return false;
+            }
+
+            return hasPathSum(root.left, targetSum-root.val) || hasPathSum(root.right,targetSum-root.val);
+        }
+    }
+}
