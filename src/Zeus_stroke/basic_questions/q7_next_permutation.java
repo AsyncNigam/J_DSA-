765a09e2c274ee672a0c4f77ccc474eb5f7e6027
NIGAM PRASAD SAHOO <nigam.nps@gmail.com>
2026-09-08 21:05:28 +0530

    31. Next Permutation

---
+++ src/Zeus_stroke/basic_questions/q7_next_permutation.java
@@ -0,0 +1,51 @@
+package Zeus_stroke.basic_questions;
+
+public class q7_next_permutation {
+    class Solution {
+        public void nextPermutation(int[] nums) {
+            int n = nums.length;
+            int pivot = -1;
+
+            // 1. Find the breakpoint
+            for (int i = n - 2; i >= 0; i--) {
+                if (nums[i] < nums[i + 1]) {
+                    pivot = i;
+                    break;
+                }
+            }
+
+            // 2. If no breakpoint, it's the last permutation. Reverse and exit.
+            if (pivot == -1) {
+                reverse(0, n - 1, nums);
+                return;
+            }
+
+            // 3. Find the next greater element index from the right
+            int swapIndex = -1;
+            for (int i = n - 1; i > pivot; i--) {
+                if (nums[i] > nums[pivot]) {
+                    swapIndex = i;
+                    break;
+                }
+            }
+
+            // 4. Swap and reverse the tail
+            swap(swapIndex, pivot, nums);
+            reverse(pivot + 1, n - 1, nums);
+        }
+
+        void reverse(int s, int e, int[] nums) {
+            while (s < e) {
+                swap(s, e, nums);
+                s++;
+                e--;
+            }
+        }
+
+        void swap(int i, int j, int[] nums) {
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+        }
+    }
+}
---
+++ src/Zeus_stroke/q9_dp/q2_distinct_subsequences_2.java
@@ -0,0 +1,24 @@
+package Zeus_stroke.q9_dp;
+
+public class q2_distinct_subsequences_2 {
+//    brute force approach
+
+//    class Solution {
+//    public int distinctSubseqII(String s) {
+//        HashSet<String> set=new HashSet<>();
+//        helper(s,"",0,set);
+//        return set.size()-1;
+//    }
+//    void helper(String s, String curr, int i, HashSet set){
+//        if(i==s.length()){
+//            set.add(curr);
+//            return;
+//        }
+//
+//        helper(s,curr+s.charAt(i),i+1,set);
+//        helper(s, curr, i+1, set);
+//    }
+//}
+
+
+}
