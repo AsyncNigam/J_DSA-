ff683f6c1d01dd62fce1d3fddd89fa47be1989c7
NIGAM PRASAD SAHOO <nigam.nps@gmail.com>
2026-09-18 02:24:39 +0530

    29. Divide Two Integers

---
+++ src/Bit_manipulation/q8_divide_two_integers.java
@@ -0,0 +1,34 @@
+package Bit_manipulation;
+
+//https://leetcode.com/problems/divide-two-integers/description/
+
+public class q8_divide_two_integers {
+    class Solution {
+        public int divide(int dividend, int divisor) {
+            if (dividend == Integer.MIN_VALUE && divisor == -1) {
+                return Integer.MAX_VALUE;
+            }
+
+            boolean isNegative = (dividend < 0) ^ (divisor < 0);
+
+            int negDividend = dividend < 0 ? dividend : -dividend;
+            int negDivisor = divisor < 0 ? divisor : -divisor;
+
+            int quotient = 0;
+
+            while (negDividend <= negDivisor) {
+                int currentDivisor = negDivisor;
+                int multiple = -1;
+                while (currentDivisor >= (Integer.MIN_VALUE >> 1) && negDividend <= (currentDivisor << 1)) {
+                    currentDivisor <<= 1;
+                    multiple <<= 1;
+                }
+
+                negDividend -= currentDivisor;
+                quotient += multiple;
+            }
+
+            return isNegative ? quotient : -quotient;
+        }
+    }
+}
