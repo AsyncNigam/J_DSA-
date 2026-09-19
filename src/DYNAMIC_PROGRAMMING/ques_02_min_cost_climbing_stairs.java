de21c37f6e1d9a18c7b6df997d2e89d0ea8a3f50
NIGAM PRASAD SAHOO <nigam.nps@gmail.com>
2026-09-19 23:15:41 +0530

    min cost climbing stairs revision

--- src/DYNAMIC_PROGRAMMING/ques_02_min_cost_climbing_stairs.java
+++ src/DYNAMIC_PROGRAMMING/ques_02_min_cost_climbing_stairs.java
@@ -5,21 +5,23 @@ import java.util.Arrays;
 public class ques_02_min_cost_climbing_stairs {
 
 //    this is the basic recursive approach , but here time limit exceeds
+//    actually in the question given to climbing stairs but we can go down the stairs
 
-//    class Solution {
-//        public int minCostClimbingStairs(int[] cost) {
-//            int n=cost.length;
-//            return Math.min(minCost(n-1,cost),minCost(n-2,cost));
-//        }
-//
-//        int minCost(int i,int[] arr){
-//            if(i<=1)return arr[i];
-//            return arr[i]+Math.min(minCost(i-1,arr),minCost(i-2,arr));
-//        }
-//    }
+//    whenever in the question given to climb the stairs you can go down the stair
+    class Solution1 {
+        public int minCostClimbingStairs(int[] cost) {
+            int n=cost.length;
+            return Math.min(minCost(n-1,cost),minCost(n-2,cost));
+        }
+
+        int minCost(int i,int[] arr){
+            if(i<=1)return arr[i];
+            return arr[i]+Math.min(minCost(i-1,arr),minCost(i-2,arr));
+        }
+    }
 
 //    by memoization
-class Solution {
+class Solution2 {
     public int minCostClimbingStairs(int[] cost) {
         int n=cost.length;
         int[] dp=new int[n];
@@ -37,19 +39,19 @@ class Solution {
 
 //by tabulation method
 
-//    class Solution {
-//    public int minCostClimbingStairs(int[] cost) {
-//        int n=cost.length;
-//        int[] dp=new int[n];
-//        dp[0]=cost[0];
-//        dp[1]=cost[1];
-//
-//        for(int i=2;i<n;i++){
-//            dp[i]=cost[i]+Math.min(dp[i-1],dp[i-2]);
-//        }
-//        return Math.min(dp[n-1],dp[n-2]);
-//    }
-//}
+    class Solution3 {
+    public int minCostClimbingStairs(int[] cost) {
+        int n=cost.length;
+        int[] dp=new int[n];
+        dp[0]=cost[0];
+        dp[1]=cost[1];
+
+        for(int i=2;i<n;i++){
+            dp[i]=cost[i]+Math.min(dp[i-1],dp[i-2]);
+        }
+        return Math.min(dp[n-1],dp[n-2]);
+    }
+}
 
 
 }
